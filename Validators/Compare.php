40132dd43ace872bdb0f9f2ef5db05dc7de4d99e
Dinookys <dinookys@gmail.com>
2019-04-30 18:53:55 -0300

    Add Compare

---
+++ Validators/Compare.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Validators;
+
+
+use Classes\Validator;
+
+/**
+ * @param string $compare field name for comparation 
+ * @var $message using method sprintf for replace %s for label or placholder of field comparation name
+ */
+class Compare extends Validator
+{
+    protected $compare;
+    protected $message = 'O valor do campo deve ser igual ao campo: "%s"';
+
+    public function __construct($compare = null)
+    {
+        if ($compare == null) {
+            throw new \Exception('Need set compare field name');
+        }
+
+        $this->compare = $compare;
+    }
+
+    public function validation($value, \Form\Form $instance)
+    {
+        $data_compare = $instance->getData()[$this->compare];
+
+        $field_compare = $instance->getField($this->compare)[0];
+        
+        //Get Field Attrs;
+        $this->setMessage(sprintf($this->message, $field_compare['label'] ?? $field_compare['placeholder']));
+        
+        if ($value != $data_compare) {
+            return false;
+        }
+
+        return true;
+    }    
+}
